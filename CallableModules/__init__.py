87d07c014ac10d228863a8e319fd7c3b9fb5fce4
Riley Flynn <riley@rileyflynn.me>
2018-03-28 18:03:48 -0230

    Made CallableModules callable

--- CallableModules/__init__.py
+++ CallableModules/__init__.py
@@ -9,17 +9,27 @@ class CallableModule(types.ModuleType):
         super(CallableModule, self).__init__(name)
         self.old_module = old_module
         self.method_name = method_name
-    
+
     def __getattr__(self, name):
         return getattr(sys.modules[self.old_module], name)
 
     def __call__(self, *args, **kwargs):
         return getattr(sys.modules[self.old_module], self.method_name)(*args, **kwargs)
 
-def patch(module_name = None, method_name = "__call__"):
+
+def patch(module_name=None, method_name="__call__"):
     if module_name is None:
         module_name = inspect.currentframe().f_back.f_globals["__name__"]
 
     oldmodule = sys.modules[module_name]
     sys.modules[module_name] = CallableModule(module_name, module_name + "_OLD", method_name)
     sys.modules[module_name + "_OLD"] = oldmodule
+
+
+def _callable_patch(module_name=None, method_name="__call__"):
+    if module_name is None:
+        module_name = inspect.currentframe().f_back.f_back.f_globals["__name__"]
+    patch(module_name, method_name)
+
+
+patch(module_name="CallableModules", method_name="_callable_patch")
--- TestModule.py
+++ TestModule.py
@@ -1,9 +1,11 @@
-from CallableModules import patch
+import CallableModules
+
 
 def __call__(*args, **kwargs):
     print(args, kwargs)
 
+
 a = 1
 b = 2
 c = 3
-patch()
+CallableModules()
